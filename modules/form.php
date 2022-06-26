b8d18480780a9efb2c9aa47480bd219cd627d5bd
Chris <banksjazz87@gmail.com>
2022-06-26 17:55:56 -0400

    chore: I have added a method to clear all of the user input and I have started messing with styling with bootstrap

--- modules/form.php
+++ modules/form.php
@@ -1,24 +1,24 @@
 
 <!DOCTYPE html>
 <html>
-<form method="post" action="/public/index.php">
-    <label for="name">Name:</label>
-    <input type="text" name="name"/>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> 
+    </head>
+<style>
+    <?php require "../assets/form.css" ?>
+</style>
+<form id="main_form" method="post" action="/public/index.php" class="col-lg-1" >
+    <input type="text" name="name" placeholder="Name"/>
     <br/>
 
-    <label for="number">Phone Number:</label>
-    <input type="text" name="number"/>
+    <input type="text" name="number" placeholder="Number"/>
     <br/>
 
-    <input type="submit" value="Submit"/>
-
-    <?php 
-        if (isset($_POST['submit'])) {
-           echo "Yes";
-        }
-        
-    ?>
+    <input type="submit" value="Submit" class="submit"/>
+    <input type="submit" value="Clear" name="clear" class="submit"/>
 
 </form>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </html>
--- public/index.php
+++ public/index.php
@@ -10,17 +10,35 @@
         } 
     }
     session_start();
-    if (!isset($_SESSION["arrContacts"])) {
+
+    function checkForCurrentContacts(){
+    if ( !isset($_SESSION["arrContacts"]) ) {
         $_SESSION["arrContacts"] = array();
+        $currentContact = new Contact($_POST["name"], $_POST["number"]);
+        array_push($_SESSION["arrContacts"], $currentContact);
     } else {
         if ( searchForValue($_SESSION["arrContacts"], $_POST["name"] )){
             echo "<script type='text/javascript' language='javascript'>alert('Contact already exists in your contacts');</script>";
         } else {
-        $currentContact = new Contact($_POST["name"], $_POST["number"]);
+        $currentContact = new Contact( $_POST["name"], $_POST["number"] );
         array_push($_SESSION["arrContacts"], $currentContact);
        
         }
     }
+}
+
+
+    checkForCurrentContacts();
+
+    function checkForClearRequest(){
+        if ( isset($_POST["clear"]) ) {
+            session_destroy();
+            unset($_POST["clear"]);
+        }
+    }
+
+    checkForClearRequest();
+
 ?>
 <?php
     //declare(strict_types=1);
