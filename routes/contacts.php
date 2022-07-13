a40189292c92c1826cdcecd3490276a3bfd438b5
Chris <banksjazz87@gmail.com>
2022-07-12 21:56:21 -0400

    chore: I worked on some styling on the edit and delete buttons on the contacts page

--- routes/contacts.php
+++ routes/contacts.php
@@ -16,6 +16,11 @@
     $formTests->checkForCurrentContacts();
     $formTests->checkForClearRequest();
 ?>
+<!-- Bootstrap stuff -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 <link href="../assets/contacts.css" rel="stylesheet" />
 <script type="text/javascript" src="../js/contactEvents.js"></script>
@@ -31,8 +36,8 @@
 ?>
             <p class="contact_info" onclick="contactEvents.initialClick()"> <?php echo $contact->get_name() . " " . $contact->get_number()?></p> 
             <div id="edit_button_wrapper" style="display: none;">
-                <button>Edit</button>
-                <button>Delete</button>
+                <button type="button" class="btn btn-small edit_button">Edit</button>
+                <button type="button" class="btn btn-outline-warning btn-small edit_button">Delete</button>
             </div> 
 <?php  
         }
